6b188dd486d33040b120017c9e30ce1f4052d574
Connor Tyrrell <connort@slalom.com>
2019-10-30 09:50:55 -0400

    Added a List all Images and Delete (actually deactivates) Image

--- api/image.js
+++ api/image.js
@@ -70,10 +70,71 @@ module.exports.get = (event, context, callback) => {
   dynamoDb.scan(params, onScan);
 };
 
+module.exports.delete = (event, context, callback) => {
+  const imageId = event.pathParameters.id;
+
+	if (typeof imageId !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t delete image because of validation errors.'));
+    return;
+  }
+
+  deleteImageP(imageId)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully deleted Image with id ${imageId}`
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to delete Image with id ${imageId}`
+        })
+      })
+    });
+};
+
+module.exports.list = (event, context, callback) => {
+  var params = {
+    TableName: process.env.IMAGE_TABLE,
+    ProjectionExpression: "imageId, imageUrl, submittedAt, updatedAt",
+    FilterExpression: 'activeFlag = :active',
+    ExpressionAttributeValues : {
+      ':active': true,
+    }
+  };
+
+  console.log("Scanning Image table");
+  const onScan = (err, data) => {
+    if (err) {
+      console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+      callback(err);
+    } else {
+      console.log("Scan succeeded.");
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          users: data.Items
+        })
+      });
+    }
+  };
+  dynamoDb.scan(params, onScan);
+};
+
 const submitImageP = (image) => {
+  const timestamp = new Date().getTime();
   const imageData = {
     imageId: uuid.v1(),
     imageUrl: image,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+		activeFlag: true,
   }
 
   console.log('Submitting Image');
@@ -83,4 +144,22 @@ const submitImageP = (image) => {
   };
   return dynamoDb.put(imageInfo).promise()
     .then(res => imageData);
+};
+
+const deleteImageP = (imageId) => {
+	console.log('Deleting Image');
+	const timestamp = new Date().getTime();
+  const imageInfo = {
+    TableName: process.env.IMAGE_TABLE,
+		Key: {
+			"imageId": imageId
+		},
+    UpdateExpression: "set activeFlag = :y, updatedAt = :z",
+		ExpressionAttributeValues: {
+			":y": false,
+      ":z": timestamp,
+    }
+  };
+	return dynamoDb.update(imageInfo).promise()
+		.then(res => timestamp);
 };
